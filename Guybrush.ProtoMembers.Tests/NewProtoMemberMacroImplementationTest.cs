5b981047b60a75dc65828ed14b18a261daedac84
Alexander Svyazin <svyazin@skbkontur.ru>
2015-07-27 15:21:33 +0500

    added test project, doesn't work yet

---
+++ Guybrush.ProtoMembers.Tests/NewProtoMemberMacroImplementationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Macros;
+using JetBrains.ReSharper.FeaturesTestFramework.LiveTemplates;
+using NUnit.Framework;
+
+namespace Guybrush.ProtoMembers.Tests
+{
+	[TestFixture]
+	public class NewProtoMemberMacroImplementationTest: MacroImplTestBase
+	{
+		protected override IMacroImplementation GetMacro(IEnumerable<IMacroParameterValueNew> parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void Test1()
+		{
+			DoOneTest("test1");
+		}
+	}
+}
---
+++ Guybrush.ProtoMembers.Tests/ProtoMembersTestEnvironmentAssembly.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Guybrush.ProtoMembers;
+using JetBrains.Application;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Resources.Shell;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using JetBrains.Threading;
+using NUnit.Framework;
+
+[SetUpFixture]
+public class ProtoMembersTestEnvironmentAssembly : TestEnvironmentAssembly<TestEnvironmentZone>
+{
+	private static IEnumerable<Assembly> GetAssembliesToLoad()
+	{
+		// Test assembly
+		yield return Assembly.GetExecutingAssembly();
+		yield return typeof (NewProtoMemberMacroImplementation).Assembly;
+	}
+
+	public override void SetUp()
+	{
+		base.SetUp();
+		ReentrancyGuard.Current.Execute(
+			"LoadAssemblies",
+			() => Shell.Instance.GetComponent<AssemblyManager>().LoadAssemblies(
+				GetType().Name,
+				GetAssembliesToLoad()));
+	}
+
+	public override void TearDown()
+	{
+		ReentrancyGuard.Current.Execute(
+			"UnloadAssemblies",
+			() => Shell.Instance.GetComponent<AssemblyManager>().UnloadAssemblies(
+				GetType().Name,
+				GetAssembliesToLoad()));
+		base.TearDown();
+	}
+}
+
+[ZoneDefinition]
+public class TestEnvironmentZone : ITestsZone
+{
+}
